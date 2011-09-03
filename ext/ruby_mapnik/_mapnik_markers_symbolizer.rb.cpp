e3c41d0a6c7441fbda1a59997f4b1a74e0ca0bc8
Elliot Laster <elliotlaster@gmail.com>
2011-09-03 11:45:13 -0400

    Documentation for markers and point symbolizer

--- ext/ruby_mapnik/_mapnik_markers_symbolizer.rb.cpp
+++ ext/ruby_mapnik/_mapnik_markers_symbolizer.rb.cpp
@@ -8,43 +8,111 @@
 // Mapnik
 #include <mapnik/markers_symbolizer.hpp>
 
-const std::string get_filename(mapnik::markers_symbolizer const& t) { 
-    return mapnik::path_processor_type::to_string(*t.get_filename()); 
-}
+namespace {
+  const std::string get_filename(mapnik::markers_symbolizer const& t) { 
+      return mapnik::path_processor_type::to_string(*t.get_filename()); 
+  }
 
-void set_filename(mapnik::markers_symbolizer & t, std::string const& file_expr) { 
-    t.set_filename(mapnik::parse_path(file_expr)); 
-}
+  void set_filename(mapnik::markers_symbolizer & t, std::string const& file_expr) { 
+      t.set_filename(mapnik::parse_path(file_expr)); 
+  }
 
-const double get_markers_opacity(mapnik::markers_symbolizer const& t){
-  return t.get_opacity();
-}
+  const double get_markers_opacity(mapnik::markers_symbolizer const& t){
+    return t.get_opacity();
+  }
 
-void set_markers_opacity(mapnik::markers_symbolizer & t, double value){
-  t.set_opacity(value);
+  void set_markers_opacity(mapnik::markers_symbolizer & t, double value){
+    t.set_opacity(value);
+  }
 }
-
 void register_markers_symbolizer(Rice::Module rb_mapnik){
   /*
     @@Module_var rb_mapnik = Mapnik
   */
   Rice::Data_Type< mapnik::markers_symbolizer > rb_cmarkers_symbolizer = Rice::define_class_under< mapnik::markers_symbolizer >(rb_mapnik, "MarkersSymbolizer");
+  
+  /*
+  * Document-method: new
+  * call-seq:
+  *  new(path_expression)
+  * @param [Mapnik::PathExpression]
+  * @return [Mapnik::MarkersSymbolizer]
+  */
   rb_cmarkers_symbolizer.define_constructor(Rice::Constructor< mapnik::markers_symbolizer, mapnik::path_expression_ptr>());
   
+  /*
+  * Document-method: filename
+  * @return [String]
+  */
   rb_cmarkers_symbolizer.define_method("filename", &get_filename);
+  
+  /*
+  * Document-method: filename=
+  * call-seq:
+  *   filename=(new_filename)
+  * @param [String] filename
+  * @return [nil]
+  */
   rb_cmarkers_symbolizer.define_method("filename=", &set_filename);
   
+  /*
+  * Document-method: allow_overlap?
+  * @return [Boolean]
+  */
   rb_cmarkers_symbolizer.define_method("allow_overlap?", &mapnik::markers_symbolizer::get_allow_overlap);
+  
+  /*
+  * Document-method: allow_overlap=
+  * call-seq:
+  *   allow_overlap=(new_value)
+  * @param [Boolean] new_value
+  * @return [nil]
+  */
   rb_cmarkers_symbolizer.define_method("allow_overlap=", &mapnik::markers_symbolizer::set_allow_overlap);
   
+  /*
+  * Document-method: spacing
+  * @return [Integer]
+  */
   rb_cmarkers_symbolizer.define_method("spacing", &mapnik::markers_symbolizer::get_spacing);
+  
+  /*
+  * Document-method: spacing=
+  * call-seq:
+  *   spacing=(new_spacing_value)
+  * @param [Integer] new_spacing_value
+  * @return [nil]
+  */
   rb_cmarkers_symbolizer.define_method("spacing=", &mapnik::markers_symbolizer::set_spacing);
   
+  /*
+  * Document-method: max_error
+  * @return [Integer]
+  */
   rb_cmarkers_symbolizer.define_method("max_error", &mapnik::markers_symbolizer::get_max_error);
+  
+  /*
+  * Document-method:max_error=
+  * call-seq: 
+  *   max_error=(new_value)
+  * @param [Integer] new_value
+  * @return [nil]
+  */
   rb_cmarkers_symbolizer.define_method("max_error=", &mapnik::markers_symbolizer::set_max_error);
   
+  /*
+  * Document-method: opacity
+  * @return [Float]
+  */
   rb_cmarkers_symbolizer.define_method("opacity", &get_markers_opacity);
-  rb_cmarkers_symbolizer.define_method("opacity=", &set_markers_opacity);
   
+  /*
+  * Document-method: opacity=
+  * call-seq:
+  *   opacity=(new_value)
+  * @param [Float]
+  * @return [nil]
+  */
+  rb_cmarkers_symbolizer.define_method("opacity=", &set_markers_opacity);
   
 }
--- ext/ruby_mapnik/_mapnik_point_symbolizer.rb.cpp
+++ ext/ruby_mapnik/_mapnik_point_symbolizer.rb.cpp
@@ -9,32 +9,36 @@
 // Mapnik
 #include <mapnik/point_symbolizer.hpp>
 
-const std::string get_filename(mapnik::point_symbolizer const& t) { 
-    return mapnik::path_processor_type::to_string(*t.get_filename()); 
-}
+namespace {
 
-void set_filename(mapnik::point_symbolizer & t, std::string const& file_expr) { 
-    t.set_filename(mapnik::parse_path(file_expr)); 
-}
+  const std::string get_filename(mapnik::point_symbolizer const& t) { 
+      return mapnik::path_processor_type::to_string(*t.get_filename()); 
+  }
 
-const double get_point_opacity(mapnik::point_symbolizer const& t){
-  return t.get_opacity();
-}
+  void set_filename(mapnik::point_symbolizer & t, std::string const& file_expr) { 
+      t.set_filename(mapnik::parse_path(file_expr)); 
+  }
 
-void set_point_opacity(mapnik::point_symbolizer & t, double value){
-  t.set_opacity(value);
-}
+  const double get_point_opacity(mapnik::point_symbolizer const& t){
+    return t.get_opacity();
+  }
 
-mapnik::point_placement_enum get_placement(mapnik::point_symbolizer const& t){
-  return t.get_point_placement();
-}
+  void set_point_opacity(mapnik::point_symbolizer & t, double value){
+    t.set_opacity(value);
+  }
 
-void set_placement(mapnik::point_symbolizer & t, mapnik::point_placement_enum value){
-  t.set_point_placement(value);
-}
+  mapnik::point_placement_enum get_placement(mapnik::point_symbolizer const& t){
+    return t.get_point_placement();
+  }
 
-std::string get_point_transform_string(mapnik::point_symbolizer const& t){
-  return t.get_transform_string();
+  void set_placement(mapnik::point_symbolizer & t, mapnik::point_placement_enum value){
+    t.set_point_placement(value);
+  }
+
+  std::string get_point_transform_string(mapnik::point_symbolizer const& t){
+    return t.get_transform_string();
+  }
+
 }
 
 void register_point_symbolizer(Rice::Module rb_mapnik){
@@ -47,18 +51,69 @@ void register_point_symbolizer(Rice::Module rb_mapnik){
   placement_enum.define_value("INTERIOR",mapnik::INTERIOR_POINT_PLACEMENT);
   
   Rice::Data_Type< mapnik::point_symbolizer > rb_cpoint_symbolizer = Rice::define_class_under< mapnik::point_symbolizer >(rb_mapnik, "PointSymbolizer");
+  
+  /*
+  * Document-method: new
+  * @return [Mapnik::PointSymbolizer]
+  */
   rb_cpoint_symbolizer.define_constructor(Rice::Constructor< mapnik::point_symbolizer >());
   
+  /*
+  * Document-method: filename
+  * @return [String]
+  */
   rb_cpoint_symbolizer.define_method("filename", &get_filename);
+  
+  /* Document-method: filename=
+  * call-seq:
+  *   filename=(new_filename)
+  * @param [String] filename
+  * @return [nil]
+  */
   rb_cpoint_symbolizer.define_method("filename=", &set_filename);
   
+  /*
+  * Document-method: allow_overlap?
+  * @return [Boolean]
+  */
   rb_cpoint_symbolizer.define_method("allows_overlap?", &mapnik::point_symbolizer::get_allow_overlap);
+  
+  /*
+  * Document-method: allow_overlap=
+  * call-seq:
+  *   allow_overlap=(new_value)
+  * @param [Boolean] new_value
+  * @return [nil]
+  */
   rb_cpoint_symbolizer.define_method("allows_overlap=", &mapnik::point_symbolizer::set_allow_overlap);
   
+  /*
+  * Document-method: opacity
+  * @return [Float]
+  */
   rb_cpoint_symbolizer.define_method("opacity", &get_point_opacity);
+  
+  /*
+  * Document-method: opacity=
+  * call-seq:
+  *   opacity=(new_value)
+  * @param [Float]
+  * @return [nil]
+  */
   rb_cpoint_symbolizer.define_method("opacity=", &set_point_opacity);
   
+  /*
+  * Document-method: ignores_placement?
+  * @return [Boolean]
+  */
   rb_cpoint_symbolizer.define_method("ignores_placement?", &mapnik::point_symbolizer::get_ignore_placement);
+  
+  /*
+  * Document-method: ignores_placement=
+  * call-seq:
+  *   ignores_placement=(new_value)
+  * @param [Boolean] new_value
+  */
   rb_cpoint_symbolizer.define_method("ignores_placement=", &mapnik::point_symbolizer::set_ignore_placement);
   
   rb_cpoint_symbolizer.define_method("placement", &get_placement);
