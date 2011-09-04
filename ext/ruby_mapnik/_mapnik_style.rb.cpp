3713dde1ff5ea6e3741be7c9728846a358af7f3c
Elliot Laster <elliotlaster@gmail.com>
2011-09-04 11:44:45 -0400

    Basic documentation for style

--- ext/ruby_mapnik/_mapnik_style.rb.cpp
+++ ext/ruby_mapnik/_mapnik_style.rb.cpp
@@ -16,23 +16,36 @@ Rice::Object to_ruby<std::vector<mapnik::rule> >(std::vector<mapnik::rule> const
   return Rice::Array(x.begin(), x.end());
 }
 
-void set_style_filter(mapnik::feature_type_style& self, mapnik::filter_mode_enum value){
-  mapnik::filter_mode_e v(value);
-  self.set_filter_mode(v);
-}
+namespace {
 
-mapnik::filter_mode_enum get_style_filter(const mapnik::feature_type_style& self){
-  return self.get_filter_mode();
-}
+  void set_style_filter(mapnik::feature_type_style& self, mapnik::filter_mode_enum value){
+    mapnik::filter_mode_e v(value);
+    self.set_filter_mode(v);
+  }
 
+  mapnik::filter_mode_enum get_style_filter(const mapnik::feature_type_style& self){
+    return self.get_filter_mode();
+  }
+
+}
 void register_style(Rice::Module rb_mapnik){
   /*
     @@Module_var rb_mapnik = Mapnik
   */
   Rice::Data_Type< mapnik::feature_type_style > rb_cstyle = Rice::define_class_under< mapnik::feature_type_style >(rb_mapnik, "Style");
+  
+  /*
+  * Document-method: new
+  * @return [Mapnik::Style]
+  */
   rb_cstyle.define_constructor(Rice::Constructor< mapnik::feature_type_style >());
+  
+  // Dont-Document-method: __rules__
   rb_cstyle.define_method("__rules__", &mapnik::feature_type_style::get_rules);
+  
+  // Dont-Document-method: __add_rule__
   rb_cstyle.define_method("__add_rule__", &mapnik::feature_type_style::add_rule);
+  
   rb_cstyle.define_method("filter_mode=", &set_style_filter);
   rb_cstyle.define_method("filter_mode", &get_style_filter);
 
