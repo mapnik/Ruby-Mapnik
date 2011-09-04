6e5ed0f8788c76ca873c4f6cc9608f6042ee6063
Elliot Laster <elliotlaster@gmail.com>
2011-09-04 11:46:57 -0400

    Added vasic documentation for symbolizer

--- ext/ruby_mapnik/_mapnik_symbolizer.rb.cpp
+++ ext/ruby_mapnik/_mapnik_symbolizer.rb.cpp
@@ -9,38 +9,48 @@
 // Mapnik
 #include <mapnik/rule.hpp>
 
-// TODO: Hackasaurus!
-mapnik::symbolizer from_subtype(Rice::Object obj){
-  mapnik::symbolizer out;
-  std::string class_name = obj.class_of().to_s().str();
-  if(class_name == "Mapnik::PolygonSymbolizer") {
-    out = mapnik::symbolizer(from_ruby<mapnik::polygon_symbolizer>(obj));
-  } else if(class_name == "Mapnik::LineSymbolizer") {
-    out = mapnik::symbolizer(from_ruby<mapnik::line_symbolizer>(obj));
-  } else if(class_name == "Mapnik::TextSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::text_symbolizer>(obj));
-  } else if(class_name == "Mapnik::GlyphSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::glyph_symbolizer>(obj));
-  } else if(class_name == "Mapnik::LinePatternSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::line_pattern_symbolizer>(obj));
-  } else if(class_name == "Mapnik::MarkersSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::markers_symbolizer>(obj));
-  } else if(class_name == "Mapnik::PointSymbolizer") {
-    out = mapnik::symbolizer(from_ruby<mapnik::point_symbolizer>(obj));
-  } else if(class_name == "Mapnik::PolygonPatternSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::polygon_pattern_symbolizer>(obj));
-  } else if(class_name == "Mapnik::RasterSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::raster_symbolizer>(obj));
-  } else if(class_name == "Mapnik::ShieldSymbolizer"){
-    out = mapnik::symbolizer(from_ruby<mapnik::shield_symbolizer>(obj));
-  }
-  return out;
-}
+namespace {
 
+  // TODO: This is just terrible...
+  mapnik::symbolizer from_subtype(Rice::Object obj){
+    mapnik::symbolizer out;
+    std::string class_name = obj.class_of().to_s().str();
+    if(class_name == "Mapnik::PolygonSymbolizer") {
+      out = mapnik::symbolizer(from_ruby<mapnik::polygon_symbolizer>(obj));
+    } else if(class_name == "Mapnik::LineSymbolizer") {
+      out = mapnik::symbolizer(from_ruby<mapnik::line_symbolizer>(obj));
+    } else if(class_name == "Mapnik::TextSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::text_symbolizer>(obj));
+    } else if(class_name == "Mapnik::GlyphSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::glyph_symbolizer>(obj));
+    } else if(class_name == "Mapnik::LinePatternSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::line_pattern_symbolizer>(obj));
+    } else if(class_name == "Mapnik::MarkersSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::markers_symbolizer>(obj));
+    } else if(class_name == "Mapnik::PointSymbolizer") {
+      out = mapnik::symbolizer(from_ruby<mapnik::point_symbolizer>(obj));
+    } else if(class_name == "Mapnik::PolygonPatternSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::polygon_pattern_symbolizer>(obj));
+    } else if(class_name == "Mapnik::RasterSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::raster_symbolizer>(obj));
+    } else if(class_name == "Mapnik::ShieldSymbolizer"){
+      out = mapnik::symbolizer(from_ruby<mapnik::shield_symbolizer>(obj));
+    }
+    return out;
+  }
+
+}
 void register_symbolizer(Rice::Module rb_mapnik){
   /*
     @@Module_var rb_mapnik = Mapnik
   */
   Rice::Data_Type< mapnik::symbolizer > rb_csymbolizer = Rice::define_class_under< mapnik::symbolizer >(rb_mapnik, "Symbolizer");
+  
+  /*
+  * Document-method: from_subtype
+  * call-seq:
+  *   from_subtype(symbolizer)
+  * @return [Mapnik::Symbolizer]
+  */
   rb_csymbolizer.define_singleton_method("from_subtype", &from_subtype, Rice::Arg("obj"));
 }
