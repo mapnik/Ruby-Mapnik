6f5bb888cfa5431fdf4bbfb5072e1bb63189f28a
Dane Springmeyer <dane@dbsgeo.com>
2012-02-13 12:22:18 -0800

    adapt to mapnik refactoring - text placement and feature_impl merge - to expose mapnik::context_ptr

--- ext/ruby_mapnik/_mapnik.cpp
+++ ext/ruby_mapnik/_mapnik.cpp
@@ -47,7 +47,6 @@ void register_mapnik(){
   register_raster_colorizer(rb_mapnik);
   register_point_symbolizer(rb_mapnik);
   register_polygon_pattern_symbolizer(rb_mapnik);
-  register_glyph_symbolizer(rb_mapnik);
   register_line_pattern_symbolizer(rb_mapnik);
   register_markers_symbolizer(rb_mapnik);
   register_shield_symbolizer(rb_mapnik);
