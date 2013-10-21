03ea79bff22e3fb10a459c20115b015b650aa787
Andy Allan <andy@gravitystorm.co.uk>
2013-10-21 13:42:27 +0100

    Start work on implementing AttributeCollector

--- ext/ruby_mapnik/_mapnik_attribute_collector.rb.h
+++ ext/ruby_mapnik/_mapnik_attribute_collector.rb.h
@@ -19,35 +19,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
 SOFTWARE.
  *****************************************************************************/
-#include "_mapnik.h"
+#ifndef PIKA2_MAPNIK_ATTRIBUTE_COLLECTOR
+#define PIKA2_MAPNIK_ATTRIBUTE_COLLECTOR
 
-void register_mapnik(){
-  Rice::Module rb_mapnik = Rice::define_module("Mapnik");
-  
-  register_color(rb_mapnik);
-  register_coord(rb_mapnik);
-  register_datasource(rb_mapnik);
-  register_datasource_cache(rb_mapnik);
-  register_envelope(rb_mapnik);
-  register_feature(rb_mapnik);
-  register_polygon_symbolizer(rb_mapnik);
-  register_line_symbolizer(rb_mapnik);
-  register_stroke(rb_mapnik);
-  register_rule(rb_mapnik);
-  register_expression(rb_mapnik);
-  register_symbolizer(rb_mapnik);
-  register_style(rb_mapnik);
-  register_layer(rb_mapnik);
-  register_map(rb_mapnik);
-  register_projection(rb_mapnik);
-  register_text_symbolizer(rb_mapnik);
-  register_font_engine(rb_mapnik);
-  register_raster_symbolizer(rb_mapnik);
-  register_raster_colorizer(rb_mapnik);
-  register_point_symbolizer(rb_mapnik);
-  register_polygon_pattern_symbolizer(rb_mapnik);
-  register_line_pattern_symbolizer(rb_mapnik);
-  register_markers_symbolizer(rb_mapnik);
-  register_shield_symbolizer(rb_mapnik);
-  register_attribute_collector(rb_mapnik);
+namespace Rice
+{
+ class Module; 
 }
+
+void register_attribute_collector(Rice::Module rb_mapnik);
+
+#endif
